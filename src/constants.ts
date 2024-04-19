4702963a12431755e225957f1fd5b482a5b07e0a
Łukasz Tlałka <bebiks.priv@gmail.com>
2024-04-19 23:27:30 +0200

    Update constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
 export const CURRENT_VERSION = "v1.0",
   BRANCH = "dev",
-  VERSION_CHECK_URL = `https://raw.githubusercontent.com/bebiksior/EvenBetterExtensions/${BRANCH}/version.txt`,
-  EXTENSIONS_URL = `https://raw.githubusercontent.com/bebiksior/EvenBetter/${BRANCH}/extensions.json`;
+  EXTENSIONS_URL = `https://raw.githubusercontent.com/bebiksior/EvenBetter/${BRANCH}/extensions.json`,
+  VERSION_CHECK_URL = `https://raw.githubusercontent.com/bebiksior/EvenBetterExtensions/main/version.txt`;
